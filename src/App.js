a03166905b923cce1754993a61bb5f6d9cad6035
Denis Yuzhelevsky <denislevsky@gmail.com>
2019-06-21 21:05:04 +0300

    add AppClass and compare it to App with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Denis");
+
+  function handleNameChange(e) {
+    setName(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <input value={name} onChange={(e) => setName(e.target.value)} >
+      </input>
+      <p>Hello {name}</p>
+      <p>you clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div >
   );
 }
+
+class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState((state, props) => {
+      return { count: this.state.count + 1 };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>you clicked {this.state.count} times</p>
+        <button onClick={this.handleClick}>Click me</button>
+      </div >
+    )
+  }
+}
+
 
 export default App;
